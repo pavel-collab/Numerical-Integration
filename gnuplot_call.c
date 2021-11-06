7da5e3de53dab70bb86b756577b2c4336359d1e6
pavel <rubikpf2002@mail.ru>
2021-11-06 22:10:46 +0300

    first version of gnuplot using

--- gnuplot_call.c
+++ gnuplot_call.c
@@ -1,54 +1,71 @@
-#include <unistd.h>
+//* gcc -o out -Wall -Wextra gnuplot_call.c -ldl
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
-// pipe :
-// 0 -- чтение
-// 1 -- запись
+#include <unistd.h>
+#include <dlfcn.h>
+#include <math.h>
 
 int main(int argc, char* argv[]) {
 
-    // create pipe
-    int pips_fds[2];
-    if (pipe(pips_fds) < 0) {
-        perror("failed to create pipe");
+    if (argc != 2) {
+        printf("Usage: %s <function(x)>\n", argv[0]);
         return -1;
     }
 
-    // create child process
-    pid_t child_id = fork();
-    if (child_id < 0) {
-        perror("fork");
+    // Prepare sorce file
+    FILE* f = fopen("tmp.c", "w");
+    fprintf(f,
+            "#include <math.h>\n"
+            "double tmpfun(double x) {return %s;}\n", argv[1]);
+    fclose(f);
+
+    // Call gcc
+
+    pid_t my_id;
+    if ((my_id = fork()) == 0) {
+        execlp("gcc", "gcc", "-w", "-Wall", "-fPIC", "-fPIE", "-shared", "-lm", "tmp.c", "-o", "tmp.so", NULL);
+        perror("exec");
+        return 1;
+    }
+
+    // Wait for compilation end and check its status
+
+    int status;
+    waitpid(my_id, &status, 0);
+
+    if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+        printf("[err] compilation error, gcc exit code %d\n", WEXITSTATUS(status));
+        return 2;
+    }
+
+    // Try to load compiled library
+
+    void* tmplib = dlopen("./tmp.so", RTLD_LAZY);
+    char* dlerrstr;
+    if (!tmplib) {
+        perror(dlerror());
         return -1;
     }
 
-    if (child_id == 0) {
-
-        // close parents' pipe
-        close(pips_fds[1]);
-        // redirect stdin to pipe
-        dup2(pips_fds[0], fileno(stdin));
-        close(pips_fds[0]);
-
-        // call gnuplot
-        execlp("gnuplot", "gnuplot", NULL);
-        perror("failed to exec gnuplot");
-    }
-    else {
-        close(pips_fds[0]);
-
-        // write a command for gnuplot to the pipe
-        write(pips_fds[1], "plot cos(x)", strlen("plot cos(x)"));
-        close(pips_fds[1]);
-
-        // wait the end of child process
-        wait(NULL);
+    double (*tmpfun)(double);
+    //! find out man dlopen
+    *(void **) (&tmpfun) = dlsym(tmplib, "tmpfun");
+    if ((dlerrstr = dlerror()) != NULL) {
+        puts(dlerrstr);
+        return -1;
     }
 
-    
-    exit(EXIT_SUCCESS);
+    // Try ro use it
+
+    FILE* plot_pipe = popen("gnuplot -persist", "w");
+    fputs("plot '-' w line\n", plot_pipe);
+    int i;
+    for (i = 0; i < 512; i++) {
+        double x = i * M_PI/256;
+        fprintf(plot_pipe, "%lf %lf\n", x, (*tmpfun)(x));
+    }
+
+    return 0;
 }
-
